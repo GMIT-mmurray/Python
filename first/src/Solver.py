803189a2da9fbcf94bda4aa8f8e6bfc3075ac359
Michael Murray <michael.murray@gmit.ie>
2017-02-24 08:42:40 +0000

    #2

--- first/src/Solver.py
+++ first/src/Solver.py
@@ -3,15 +3,18 @@ import math
 _author_ = 'wombat'
 
 class Solver:
-    def demo(self):
-
-        a = int(input("a "))
-        b = int(input("b "))
-        c = int(input("c "))
-        d = b ** 2 - 4 * a * c
-        disc = math.sqrt(d)
-        root1 = (-b + disc) / (2 * a)
-        root2 = (-b - disc) / (2 * a)
-        print(root1, root2)
+  def demo(self):
+      while True:
+          a = int(input("a "))
+          b = int(input("b "))
+          c = int(input("c "))
+          d = b ** 2 - 4 * a * c
+          if d>=0:
+              disc = math.sqrt(d)
+              root1 = (-b + disc) / (2 * a)
+              root2 = (-b - disc) / (2 * a)
+              print(root1, root2)
+          else:
+              print('error')
 
 Solver().demo()
